004a346d1f48ffc90fa16bf37fe85214bb281339
Wei Yuxin <weiyuxin100@gmail.com>
2018-12-10 00:14:58 +0800

    Added Mindmap Points and UNUDHR Files

---
+++ Mindmap-GP.py
@@ -0,0 +1,9 @@
+
+
+
+discrimination = {
+					'Transgender': ["Trans text", "some policy here", "Some examples here"], 
+					'Disability': ["Mental Disorders: 1, 2, 3", "Physical Incapacitation","Japan hires them to operate robots in the restaurants"]
+					}
+
+print(type(discrimination))
---
+++ UNUDHR.py
@@ -0,0 +1,5 @@
+import random
+
+for i in file.open("UNUDHR-Articles.txt", "r").readlines():
+    print("line" + i)
+
