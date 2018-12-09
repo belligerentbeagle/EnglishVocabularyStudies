e4740e364984e33cf2793b1d5b0a5848f89d5420
Wei Yuxin <weiyuxin100@gmail.com>
2018-12-10 00:42:58 +0800

    Update Mindmap-GP.py

--- Mindmap-GP.py
+++ Mindmap-GP.py
@@ -6,4 +6,18 @@ discrimination = {
 					'Disability': ["Mental Disorders: 1, 2, 3", "Physical Incapacitation","Japan hires them to operate robots in the restaurants"]
 					}
 
-print(type(discrimination))
+# class topiccaller():
+# 		def name(self):
+# 			if topic == "discrimination":
+# 				return "Discrimination"
+
+while True:
+	topic = input("Enter your topic (" + "discrimination" + "): ")
+	print("\n__" + topic.upper() + "__")
+	print("--Transgender Discrimination") # perhaps i can do regex and print a list nicely
+	for elem in discrimination['Transgender']:
+		# if elem == (discrimination['Transgender'])[-1]:
+		# 	print(str(elem))
+		# else:
+		print("----" + str(elem))
+
